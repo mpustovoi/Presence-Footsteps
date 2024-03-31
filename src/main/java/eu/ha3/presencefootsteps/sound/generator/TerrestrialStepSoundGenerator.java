4a6ab4e07d829ef522f153328eb00b23ea57f7ff
Sollace <sollacea@gmail.com>
2024-03-31 02:17:53 +0100

    Fixed slow footsteps when sprinting. Fixes #294

--- src/main/java/eu/ha3/presencefootsteps/sound/generator/TerrestrialStepSoundGenerator.java
+++ src/main/java/eu/ha3/presencefootsteps/sound/generator/TerrestrialStepSoundGenerator.java
@@ -3,6 +3,7 @@ package eu.ha3.presencefootsteps.sound.generator;
 import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.util.math.BlockPos;
@@ -177,7 +178,7 @@ class TerrestrialStepSoundGenerator implements StepSoundGenerator {
             }
 
             // Fix high speed footsteps (i.e. horse riding)
-            if (motionTracker.getHorizontalSpeed() > 0.1) {
+            if ((entity instanceof AbstractHorseEntity) && motionTracker.getHorizontalSpeed() > 0.1) {
                 distance *= 3;
             }
 
